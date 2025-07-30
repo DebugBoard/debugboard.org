d10ec808248c014e5e30bb08f7f9147f4fb36c6b
DebugBoard <admin@de4uth.org>
2025-07-30 15:28:39 +0100

    s

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
 				<link rel="manifest" href="/icons/site.webmanifest" />
 				<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#3aa1f3" />
 				<link rel="shortcut icon" href="/favicon.ico" />
